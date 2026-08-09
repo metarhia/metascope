8048a7dbbd9d3f82c4394483717f2ecd88fa5c46
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2026-08-10 01:04:37 +0300

    Initial implementation

---
+++ bin/metascope.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+'use strict';
+
+require('../lib/app')
+  .main(process.argv.slice(2))
+  .catch((err) => {
+    console.error(err);
+    process.exitCode = 1;
+  });
---
+++ test/fixtures/sample.ts
@@ -0,0 +1,3 @@
+export interface User { id: number; name: string; }
+export type Id = string | number;
+const greet = (u: User): string => `Hi ${u.name}`;
